2557ff32a9a5897b8dda8799224e76cb7a547c05
Matheus Yoshimitsu Tamashiro Pires Lanzo <matheusyoshimitsu@r09l19linux.cl.ibilce.unesp.br>
2022-10-04 11:32:18 -0300

    Comecando Trabalho de ED 2

---
+++ Estrutura-de-Dados-II/Trabalho-1/menu.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int menu(){
+    return;
+}
+    printf("| ---------------- |/n" +
+     "| MENU PRINCIPAL   |/n" +);
+    cout << "| 1 - Gravar aluno |" << endl;
+    cout << "| 2 - Buscar aluno |" << endl;
+    cout << "| 3 - Fechar       |" << endl;
+    cout << "| ---------------- |" << endl;
