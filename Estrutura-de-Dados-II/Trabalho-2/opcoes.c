df1ca7265abd67b6915a23f540d98800d9fe96b9
YoshimitsuUnesp <yoshimitsu.lanzo@unesp.br>
2022-11-12 18:51:02 -0300

    Fazer no notebook

--- Estrutura-de-Dados-II/Trabalho-2/opcoes.c
+++ Estrutura-de-Dados-II/Trabalho-2/opcoes.c
@@ -1,19 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void menuPrincipal()
-{
-    limparTela();
-    printf(
-        "| ---------------- |/n",
-        "|  MENU PRINCIPAL  |/n",
-        "| 1 - Comprimir    |/n",
-        "| 2 - Descomprimir |/n",
-        "| 3 - Sair         |/n",
-        "| ---------------- |/n");
-    printf("Escolha uma opcao (1-3):\n");
-}
-
 void sair(){
     printf("Encerrando programa...\n");
     exit(0);
@@ -33,4 +20,17 @@ void limparTela(){
     printf("\nQue sistema tu ta usando cara?\n");
     return;
     #endif
+}
+
+void menuPrincipal()
+{
+    limparTela();
+    printf(
+        "| ---------------- |\n"
+        "|  MENU PRINCIPAL  |\n"
+        "| 1 - Comprimir    |\n"
+        "| 2 - Descomprimir |\n"
+        "| 3 - Sair         |\n"
+        "| ---------------- |\n");
+    printf("\nEscolha uma opcao (1-3):\n");
 }
