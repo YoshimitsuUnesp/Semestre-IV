42189c7846bc949939f7a75748adfe98d90242b8
YoshimitsuUnesp <yoshimitsu.lanzo@unesp.br>
2022-11-17 11:54:49 -0300

    Arvore B - Avaliacao

---
+++ Lab-Estrutura-de-Dados-II/Avaliacao/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arvore-b.h"
+
+int main()
+{
+    int item, ch;
+    
+    inserir(50);
+    inserir(9);
+    inserir(1);
+    inserir(11);
+    inserir(15);
+    procura(raiz);
+}
